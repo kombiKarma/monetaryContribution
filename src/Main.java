1db62e83bc63577c16eaabbd6189b29c61eb2bd3
kombiKarma <peaswildwind@gmail.com>
2020-10-14 18:59:31 +0300

    new

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         int percent = 3;
+        double minContribution = 1000.00;
         double contribution = 10000.00;
         double income = (contribution / 100.0) * percent;
+        if (contribution < minContribution) {
+            income = 0;
+        }
         System.out.println(income);
-        //проверка репозитория
-    }
+        }
 }
